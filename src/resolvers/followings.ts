64d045dde5ee6b1aea51886da4ef8e64c7b24334
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2024-12-28 21:36:26 -0500

    Added following/ersCount functionality

--- src/resolvers/followings.ts
+++ src/resolvers/followings.ts
@@ -1,7 +1,7 @@
 import drizzleConnection from "../connection/drizzle.ts";
 import followings from "../model/followings.ts";
 const DB = drizzleConnection();
-import { sql, eq, ne, gt, gte, like, ilike, and } from "drizzle-orm";
+import { sql, eq, ne, gt, gte, like, ilike, and, count } from "drizzle-orm";
 
 
 const DEFAULT_PAGE = 1;
@@ -33,6 +33,23 @@ export const Query = {
             throw new Error('Failed to fetch followings');
         }
     },
+    followingCount: async (_parent: unknown, args: { userId: string }, ___: unknown) => {
+        try {
+            const { userId } = args;
+            
+            const followingCount = DB
+                .select({ 
+                    count: count() 
+                })
+                .from(followings)
+                .where(eq(followings.user_id, userId))
+
+            return await followingCount;
+        } catch (error) {
+            console.error('Error fetching followingCount:', error);
+            throw new Error('Failed to fetch followingCount');
+        }
+    },
     followers: async (_parent: unknown, args: { page: number, perPage: number, userId: string, createdAt: string }, ___: unknown) => {
         try {
             const { page = DEFAULT_PAGE, perPage = DEFAULT_PER_PAGE, userId, createdAt } = args;
@@ -57,6 +74,23 @@ export const Query = {
             console.error('Error fetching followings:', error);
             throw new Error('Failed to fetch followings');
         }
+    },
+    followersCount: async (_parent: unknown, args: { userId: string }, ___: unknown) => {
+        try {
+            const { userId } = args;
+            
+            const followersCount = DB
+                .select({ 
+                    count: count() 
+                })
+                .from(followings)
+                .where(eq(followings.followed_id, userId))
+
+            return await followersCount;
+        } catch (error) {
+            console.error('Error fetching followersCount:', error);
+            throw new Error('Failed to fetch followersCount');
+        }
     }
 };
 
@@ -67,9 +101,6 @@ export const Mutation = {
                 .insert(followings)
                 .values(input)
                 .returning();
-                // .onConflictDoNothing({
-                //     target: [followings.user_id, followings.following_id]
-                // });
         } catch (error) {
             if (error.code === '23505') { // PostgreSQL unique violation code
                 throw new Error('User already follows this account');
